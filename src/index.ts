1cec473cf49511af0cc31fe392c4fdb1c77e454b
saleszera <crowzcoleslaw@gmail.com>
2023-01-25 19:58:35 -0300

    chore: change rerun timer

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import path from 'path';
 
 dotenv.config({ path: path.join(__dirname, '..', '.env') });
 
-const interval = 1000 * 60 * 60 * 6;
+const interval = 1000 * 60;
 const endAt = new Date('03/03/2023').toLocaleDateString();
 
 void app({ interval, endAt });
